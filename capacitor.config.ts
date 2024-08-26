e751efdb903d3ff542295ba768f7f71821e5ed6f
Alexandre R Gomes <gomesar9@gmail.com>
2024-08-26 11:27:33 -0300

    feat: primeiro commit base para aplicativo com angular

---
+++ capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'br.gov.serpro.andat.dummyfront',
+  appName: 'dummy-front',
+  webDir: 'www'
+};
+
+export default config;
